7b00f943b5c91feb12ad50731546f757f0d7dbc6
dominiclaserna <lblaserna@up.edu.ph>
2024-05-22 21:42:15 +0800

    d

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,7 @@
 const { override, overrideDevServer } = require('customize-cra');
 
 const devServerConfig = () => config => {
-  config.allowedHosts = ['your-desired-hostname'];
+  config.allowedHosts = ['https://webapp-1-ndgn.onrender.com'];
   return config;
 };
 
